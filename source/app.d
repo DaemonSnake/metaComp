7f6e012e9efa10c9936b519312ba9a5530d0c072
Bastien Penavayre <bastienPenava@gmail.com>
2017-05-28 16:07:25 -0400

    Improve main

--- source/app.d
+++ source/app.d
@@ -4,16 +4,15 @@ import grammar.parser;
 
 void main()
 {
-    void proccess(string txt)()
+    void proccess(string txt, With = root)()
     {
         pragma(msg, "input:\n", txt, "\n");
-        enum res = root.lex!(txt, 0, "root");
+        enum res = With.lex!(txt, 0, With.stringof);
         static assert(res.state, res.msg);
         pragma(msg, "result:\n", parser!((res.data)), "\n\n");
     }
-    proccess!("root = [id:name '=' rule_body:rule]*");
-    proccess!("rule_body = ['[' [rule_element]+:content ']' ['+' | '?' | '*']?:postfix]");
-    pragma(msg, root.type.grammar_repr);
-    pragma(msg, rule_body.type.grammar_repr);
-    pragma(msg, rule_element.type.grammar_repr);
+
+    proccess!("root = " ~ root.type.grammar_repr);
+    proccess!("rule_body = " ~ rule_body.type.grammar_repr);
+    proccess!("rule_element = " ~ rule_element.type.grammar_repr);
 }
