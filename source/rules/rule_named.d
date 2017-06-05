e13cfe9e2936cc3812faf8c300988ad53875207e
Bastien Penavayre <bastienPenava@gmail.com>
2017-06-05 01:28:56 -0400

    Cosmetic

--- source/rules/rule_named.d
+++ source/rules/rule_named.d
@@ -9,6 +9,5 @@ struct named(string _name, _type)
     enum name = _name;
     alias type = _type;
     enum grammar_repr = _type.grammar_repr ~ ":" ~ _name;
-
     enum lex(string txt, size_t index) = _type.lex!(txt, index, _name);
 }
